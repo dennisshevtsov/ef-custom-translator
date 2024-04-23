5e1aea45807d8e58cbfd9af661dd3da67cdcadaf
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-23 10:38:16 -0700

    add customer translator

---
+++ src/EfCustomTranslator/CustomMethodCallTranslator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Reflection;
+
+namespace EfCustomTranslator;
+
+public sealed class CustomMethodCallTranslator(ISqlExpressionFactory sqlExpressionFactory) : IMethodCallTranslator
+{
+  private readonly ISqlExpressionFactory _sqlExpressionFactory = sqlExpressionFactory;
+
+  public SqlExpression? Translate(SqlExpression? instance, MethodInfo method, IReadOnlyList<SqlExpression> arguments, IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+  {
+    return null;
+  }
+}
