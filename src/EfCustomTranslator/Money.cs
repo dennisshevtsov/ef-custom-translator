feb72817b738551249e1e61025b7702e8109dbda
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-24 12:07:56 -0700

    add parce

--- src/EfCustomTranslator/Money.cs
+++ src/EfCustomTranslator/Money.cs
@@ -4,7 +4,28 @@
 
 namespace EfCustomTranslator;
 
-public readonly record struct Money(string Currency, ulong Units, ulong Ratio)
+public readonly record struct Money(string Currency, ulong MinorUnits)
 {
-  public override string ToString() => $"{Currency}{Units / Ratio}.{Units % Ratio}";
+  private const int MinorUnitRatio = 100;
+  private const int StringLength = 3 + 18 + 1 + 2; // 3 (currency code) + 18 (digits before point) + 1 (point) + 2 (digits after point)
+
+  public override string ToString() => $"{Currency}{MinorUnits / Money.MinorUnitRatio:D18}.{MinorUnits % Money.MinorUnitRatio:D2}"; // 18 + 2 = 20 (how many digits in ulong)
+
+  public static Money Parce(string value)
+  {
+    ArgumentNullException.ThrowIfNull(value);
+
+    if (value.Length != Money.StringLength)
+    {
+      throw new InvalidCastException($"Invalid length of string '{value}' to convert to Money");
+    }
+
+    string currency = value.Substring(0, 3);
+
+    ulong majorUnits = ulong.Parse(value.Substring(3, 18)); // 20 (all ulong digits) - 2 (digits after point) = 18
+    ulong minorUnits = ulong.Parse(value.Substring(22, 2)); // 3 (code length) + 18 (digits before point) + 1 (point) = 22
+    ulong totalMinorUnits = majorUnits * Money.MinorUnitRatio + minorUnits;
+
+    return new Money(Currency: currency, MinorUnits: totalMinorUnits);
+  }
 }
