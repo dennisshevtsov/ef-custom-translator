d7c53659f7916b6810a1ec58b70b1d1be1b3d81e
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-04-24 11:47:43 -0700

    add test

---
+++ test/EfCustomTranslator.Test/EfCustomTranslatorDbContextTest.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfCustomTranslator.Test
+{
+  [TestClass]
+  public class EfCustomTranslatorDbContextTest
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
